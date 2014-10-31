6da57f466f9b69e46bcaaf21d0b86a16303929da
Michael Entrup b. Epping <entrup@arcor.de>
2014-10-31 17:18:29 +0100

    Run the SR-EELS characterisation macro as a plugin:
    1. I created a new PlugIn to run the macro.
    2. JARTool is used to load the contend of a text file that is included
    in the jar.
    3. Moved code from Tutorial_Drift to JARTool

---
+++ EFTEMj/src/main/java/tools/JARTool.java
@@ -0,0 +1,60 @@
+package tools;
+
+import ij.IJ;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * This class is used to load data that is stored at the JAR file of this plugin package.
+ * 
+ * @author Michael Entrup b. Epping <entrup@arcor.de>
+ *
+ */
+public class JARTool {
+
+    private String path;
+
+    /**
+     * @param path
+     *            to a text file inside the JAR file.
+     */
+    public JARTool(String path) {
+	this.path = path;
+    }
+
+    /**
+     * Loads a text file from within a JAR file using getResourceAsStream().<br />
+     * Taken from <url>http://imagej.nih.gov/ij/plugins/download/JAR_Resources_Demo.java</url>.
+     * 
+     * @return Content of the text file.
+     */
+    public String getText() {
+	String text = "";
+	try {
+	    // get the text resource as a stream
+	    InputStream is = getClass().getResourceAsStream(path);
+	    if (is == null) {
+		IJ.showMessage("Load macro from JAR", "File not found in JAR at " + path);
+		return "";
+	    }
+	    InputStreamReader isr = new InputStreamReader(is);
+	    StringBuffer sb = new StringBuffer();
+	    char[] b = new char[8192];
+	    int n;
+	    // read a block and append any characters
+	    while ((n = isr.read(b)) > 0)
+		sb.append(b, 0, n);
+	    // display the text in a TextWindow
+	    text = sb.toString();
+	} catch (IOException e) {
+	    String msg = e.getMessage();
+	    if (msg == null || msg.equals(""))
+		msg = "" + e;
+	    IJ.showMessage("Load macro from JAR", msg);
+	}
+	return text;
+    }
+
+}
--- EFTEMj/src/main/java/tutorials/Tutorial_Drift.java
+++ EFTEMj/src/main/java/tutorials/Tutorial_Drift.java
@@ -1,11 +1,8 @@
 package tutorials;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import ij.IJ;
 import ij.plugin.PlugIn;
+import tools.JARTool;
 
 /**
  * This Plugin is a tutorial on how to use the drift correction.<br />
@@ -21,42 +18,7 @@ public class Tutorial_Drift implements PlugIn {
 
     @Override
     public void run(String arg) {
-	IJ.runMacro(getText(path_macros + ijm_create));
+	IJ.runMacro(new JARTool(path_macros + ijm_create).getText());
 	// TODO Create the tutorial.
-    }
-
-    /**
-     * Loads a text file from within a JAR file using getResourceAsStream().<br />
-     * Taken from <url>http://imagej.nih.gov/ij/plugins/download/JAR_Resources_Demo.java</url>.
-     * 
-     * @param path
-     *            Path to the text file.
-     * @return Content of the text file.
-     */
-    private String getText(String path) {
-	String text = "";
-	try {
-	    // get the text resource as a stream
-	    InputStream is = getClass().getResourceAsStream(path);
-	    if (is == null) {
-		IJ.showMessage("Load macro from JAR", "File not found in JAR at " + path);
-		return "";
-	    }
-	    InputStreamReader isr = new InputStreamReader(is);
-	    StringBuffer sb = new StringBuffer();
-	    char[] b = new char[8192];
-	    int n;
-	    // read a block and append any characters
-	    while ((n = isr.read(b)) > 0)
-		sb.append(b, 0, n);
-	    // display the text in a TextWindow
-	    text = sb.toString();
-	} catch (IOException e) {
-	    String msg = e.getMessage();
-	    if (msg == null || msg.equals(""))
-		msg = "" + e;
-	    IJ.showMessage("Load macro from JAR", msg);
-	}
-	return text;
     }
 }
