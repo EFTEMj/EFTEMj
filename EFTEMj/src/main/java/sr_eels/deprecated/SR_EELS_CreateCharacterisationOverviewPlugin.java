169746c003653df56eaf00665f72fc57990e4ac1
Michael Entrup b. Epping <entrup@arcor.de>
2015-02-20 14:59:03 +0100

    Updated plugins.config:
    Added the plugins that start the macros and scripts.
    
    Moved the already depreciated classes.

--- EFTEMj/src/main/java/sr_eels/deprecated/SR_EELS_CreateCharacterisationOverviewPlugin.java
+++ EFTEMj/src/main/java/sr_eels/deprecated/SR_EELS_CreateCharacterisationOverviewPlugin.java
@@ -24,7 +24,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package sr_eels.testing;
+package sr_eels.deprecated;
 
 import eftemj.EFTEMj;
 import tools.JARTool;
