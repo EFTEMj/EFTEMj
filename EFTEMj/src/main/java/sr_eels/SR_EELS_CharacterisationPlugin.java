c5c640253bab6f3b023fe452e4079cd4edf2c8a0
Michael Entrup b. Epping <entrup@arcor.de>
2015-06-17 18:06:57 +0200

    Switch to ImageJ code style profile (http://imagej.net/Coding_style).

--- EFTEMj/src/main/java/sr_eels/SR_EELS_CharacterisationPlugin.java
+++ EFTEMj/src/main/java/sr_eels/SR_EELS_CharacterisationPlugin.java
@@ -1,3 +1,4 @@
+
 package sr_eels;
 
 import eftemj.EFTEMj;
@@ -8,20 +9,22 @@ import tools.JARTool;
 
 public class SR_EELS_CharacterisationPlugin implements PlugIn {
 
-    private final String filename_ijm_Characterisation = "SR-EELS_Characterisation.ijm";
+	private final String filename_ijm_Characterisation =
+		"SR-EELS_Characterisation.ijm";
 
-    @Override
-    public void run(final String arg) {
-	IJ.runMacro(new JARTool().getText(EFTEMj.PATH_SCRIPTS_AND_MACROS + filename_ijm_Characterisation));
-    }
+	@Override
+	public void run(final String arg) {
+		IJ.runMacro(new JARTool().getText(EFTEMj.PATH_SCRIPTS_AND_MACROS +
+			filename_ijm_Characterisation));
+	}
 
-    public static void main(final String[] args) {
-	// start ImageJ
-	new ImageJ();
+	public static void main(final String[] args) {
+		// start ImageJ
+		new ImageJ();
 
-	// run the plugin
-	final Class<?> clazz = SR_EELS_CharacterisationPlugin.class;
-	IJ.runPlugIn(clazz.getName(), "");
-    }
+		// run the plugin
+		final Class<?> clazz = SR_EELS_CharacterisationPlugin.class;
+		IJ.runPlugIn(clazz.getName(), "");
+	}
 
 }
