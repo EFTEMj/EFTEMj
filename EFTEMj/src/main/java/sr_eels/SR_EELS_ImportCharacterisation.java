8d4cd3c910050f069dfa72b100603ecc44887174
Michael Entrup b. Epping <entrup@arcor.de>
2015-08-05 14:25:02 +0200

    Adding SR-EELS_ImportCharacterisation.js as plugin:
    SR_EELS_ImportCharacterisation.java runs the JavaScript file
    SR-EELS_ImportCharacterisation.js.
    
    plugins.config contains a new entry to run
    SR_EELS_ImportCharacterisation.java.

---
+++ EFTEMj/src/main/java/sr_eels/SR_EELS_ImportCharacterisation.java
@@ -0,0 +1,48 @@
+/**
+ * EFTEMj - Processing of Energy Filtering TEM images with ImageJ
+ *
+ * Copyright (c) 2015, Michael Entrup b. Epping
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package sr_eels;
+
+import eftemj.EFTEMj;
+import ij.plugin.JavaScriptEvaluator;
+import ij.plugin.PlugIn;
+import tools.JARTool;
+
+public class SR_EELS_ImportCharacterisation implements PlugIn {
+
+	private final String filename_js_ImportCharacterisation =
+		"SR-EELS_ImportCharacterisation.js";
+
+	@Override
+	public void run(final String arg) {
+		final String javaScriptCode = new JARTool().getText(
+			EFTEMj.PATH_SCRIPTS_AND_MACROS + filename_js_ImportCharacterisation);
+		final JavaScriptEvaluator jse = new JavaScriptEvaluator();
+		jse.run(javaScriptCode);
+	}
+
+}
