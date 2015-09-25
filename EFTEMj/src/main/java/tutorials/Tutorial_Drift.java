35fe0d6f3ea0a4db77a44c1ddf2755fccf74ef80
Michael Entrup <entrup@arcor.de>
2015-09-25 13:19:10 +0200

    Applying the ImageJ code style to the tutorials package.

--- EFTEMj/src/main/java/tutorials/Tutorial_Drift.java
+++ EFTEMj/src/main/java/tutorials/Tutorial_Drift.java
@@ -1,3 +1,4 @@
+
 package tutorials;
 
 import eftemj.EFTEMj;
@@ -8,27 +9,28 @@ import tools.JARTool;
 
 /**
  * This Plugin is a tutorial on how to use the drift correction.<br />
- * It will automatically create an example stack and instructs the user on how to correct the drift.
+ * It will automatically create an example stack and instructs the user on how
+ * to correct the drift.
  *
  * @author Michael Entrup b. Epping <michael.entrup@wwu.de>
- *
  */
 public class Tutorial_Drift implements PlugIn {
 
-    private final String filename_ijm_create = "create_Drift-Stack.ijm";
+	private final String filename_ijm_create = "create_Drift-Stack.ijm";
 
-    @Override
-    public void run(final String arg) {
-	IJ.runMacro(new JARTool().getText(EFTEMj.PATH_SCRIPTS_AND_MACROS + filename_ijm_create));
-	// TODO Create the tutorial.
-    }
+	@Override
+	public void run(final String arg) {
+		IJ.runMacro(new JARTool().getText(EFTEMj.PATH_SCRIPTS_AND_MACROS +
+			filename_ijm_create));
+		// TODO Create the tutorial.
+	}
 
-    public static void main(final String[] args) {
-	// start ImageJ
-	new ImageJ();
+	public static void main(final String[] args) {
+		// start ImageJ
+		new ImageJ();
 
-	// run the plugin
-	final Class<?> clazz = Tutorial_Drift.class;
-	IJ.runPlugIn(clazz.getName(), "");
-    }
+		// run the plugin
+		final Class<?> clazz = Tutorial_Drift.class;
+		IJ.runPlugIn(clazz.getName(), "");
+	}
 }
--- EFTEMj/src/main/java/tutorials/Tutorial_ESI.java
+++ EFTEMj/src/main/java/tutorials/Tutorial_ESI.java
@@ -1,36 +1,38 @@
+
 package tutorials;
+
+import java.io.InputStream;
 
 import ij.ImagePlus;
 import ij.io.Opener;
 import ij.plugin.PlugIn;
 
-import java.io.InputStream;
-
 /**
- * This Plugin is a tutorial on how to create an elemental map from energy filtered images.<br />
- * It will automatically open example images/stacks and instruct the user on how to process them using EFTEMj.
+ * This Plugin is a tutorial on how to create an elemental map from energy
+ * filtered images.<br />
+ * It will automatically open example images/stacks and instruct the user on how
+ * to process them using EFTEMj.
  *
  * @author Michael Entrup b. Epping <michael.entrup@wwu.de>
- *
  */
 public class Tutorial_ESI implements PlugIn {
 
-    private final String path = "/example-images/";
-    private final String stack_tif = "EFTEM-Stack_Fe_50counts.tif";
+	private final String path = "/example-images/";
+	private final String stack_tif = "EFTEM-Stack_Fe_50counts.tif";
 
-    @Override
-    public void run(final String arg) {
-	// Open a tiff-file that is inside the jar-file (taken from
-	// http://imagej.nih.gov/ij/plugins/download/JAR_Resources_Demo.java)
-	final InputStream is = getClass().getResourceAsStream(path + stack_tif);
-	if (is != null) {
-	    final Opener opener = new Opener();
-	    final ImagePlus imp = opener.openTiff(is, stack_tif);
-	    if (imp != null) {
-		imp.show();
-	    }
+	@Override
+	public void run(final String arg) {
+		// Open a tiff-file that is inside the jar-file (taken from
+		// http://imagej.nih.gov/ij/plugins/download/JAR_Resources_Demo.java)
+		final InputStream is = getClass().getResourceAsStream(path + stack_tif);
+		if (is != null) {
+			final Opener opener = new Opener();
+			final ImagePlus imp = opener.openTiff(is, stack_tif);
+			if (imp != null) {
+				imp.show();
+			}
+		}
+		// TODO Create the tutorial.
 	}
-	// TODO Create the tutorial.
-    }
 
 }
